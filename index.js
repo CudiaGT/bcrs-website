1cf6c284d4ce1d42ffa3b3c93110ee696f8666b6
schmidln <122846774+schmidln@users.noreply.github.com>
2024-12-01 21:03:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import renderMainPage, {getFormData} from "./main.js";
 import renderPosts from "./src/components/renderPosts.js";
 import DetailView from "./src/components/DetailView/DetailView.js"
-import renderListings from "./src/components/Listings/Listings.js";
-import {renderAbout} from "./src/components/about.js";
+import { renderListings } from "./src/components/Listings/Listings.js";
+import { renderAbout } from "./src/components/about.js";
 import { renderNavBar } from "./src/components/navbar.js";
 import { renderFooter } from "./src/components/footer.js";
 import { renderSignInPage } from "./src/components/signin.js";
