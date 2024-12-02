3d7dcae6d08f161c07fd23d3ec2641cd95d782a8
schmidln <122846774+schmidln@users.noreply.github.com>
2024-12-01 21:20:27 -0500

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@
  import { renderFooter } from "./footer.js";
 
 export function renderAbout() {
-fetch("../about.json")
+fetch("src/about.json")
   .then((response) => {
     return response.json();
   })
