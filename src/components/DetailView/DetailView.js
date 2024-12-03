1aa015d12a61163f56d5587f2ebafe6ffee6ca3e
andybello28 <108368351+andybello28@users.noreply.github.com>
2024-12-03 00:47:28 -0500

    Revert "Added Event Listeners to show more links for onCampus filtered listings. (There is a bug)"

--- src/components/DetailView/DetailView.js
+++ src/components/DetailView/DetailView.js
@@ -1,4 +1,4 @@
-import { getListingLocation, getOffCampusIdentifier, getOnCampusIdentifier } from "../Listings/renderFilteredPosts.js";
+import { getOffCampusIdentifier, getOnCampusIdentifier } from "../Listings/renderFilteredPosts.js";
 import {getFormData} from "../../../main.js";
 import renderDetailView from "./renderDetailView.js";
 
@@ -6,7 +6,7 @@ import renderDetailView from "./renderDetailView.js";
 
 
 function DetailViewOnCampus(listingLocation, data) {
-    let onCampusIdentifier = getOnCampusIdentifier(); //the ID of clicked listing 4
+    let onCampusIdentifier = getOnCampusIdentifier(); //the ID of clicked listing
     let matchingListingArray;
     console.log("onCampusIdentifier: ", onCampusIdentifier);
 
@@ -33,7 +33,7 @@ function DetailViewOffCampus(listingLocation, data) {
 
     if (listingLocation && data[listingLocation]) {
         matchingListingArray = data[listingLocation].filter(group => 
-        group.id == offCampusIdentifier
+        group.address == offCampusIdentifier
         );
     }   
     //matchingListingArray contains the singular matching listing
@@ -51,8 +51,7 @@ function DetailViewOffCampus(listingLocation, data) {
 export default function DetailView(data) {
     console.log("In DetailView.js");
 
-    //let [payload, listingLocation] = getFormData();
-    let listingLocation = getListingLocation();
+    let [payload, listingLocation] = getFormData();
     console.log("listing location:", listingLocation);
     
     if (listingLocation == "oncampus") {
